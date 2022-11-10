11d6ba077b2bb70363850bff1ea912bc844192f0
adityachandra1 <adityachandra9898@gmail.com>
2022-11-11 01:08:11 +0530

    added-jwt-middleware-auth-done

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,5 @@
 const Author = require("../models/authorModel");
 const mongoose = require("mongoose");
-const { ErrorResponse } = require('../utils/ErrorResponse')
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt");
 const session = require("sessionstorage")
@@ -8,21 +7,27 @@ const session = require("sessionstorage")
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: maxAge,
-  });
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: maxAge,
+    });
 };
-  
+
 const register = async (req, res, next) => {
     try {
         const { name, email, password, phone_no } = req.body
         let authorTest = await Author.findOne({ email });
         if (authorTest)
-            return next(new ErrorResponse("Email Already Exists", 400));
+            return res.status(401).send({
+                success: false,
+                msg: "Email Already Exists"
+            });
 
         authorTest = await Author.findOne({ phone_no });
         if (authorTest)
-            return next(new ErrorResponse("Phone Number Already Exists", 400));
+            return res.status(404).send({
+                success: false,
+                msg: "Phone Number already Exists"
+            });
 
         const author = new Author({
             name: name,
@@ -36,7 +41,11 @@ const register = async (req, res, next) => {
             `${name} registered successfully`
         );
     } catch (err) {
-        return next(new ErrorResponse(err.name, err.code));
+        console.log(err)
+        return res.status(500).send({
+            success: false,
+            msg: "Error while registering"
+        });
     }
 }
 
@@ -44,31 +53,40 @@ const login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
-            return next(new ErrorResponse('Email or Password not provided', 404));
+            return res.status(404).send({
+                success: false,
+                msg: "Email or Password is missing"
+            });
         }
         const author = await Author.findOne({ email });
         let isMatch = false;
         if (!author) {
             isMatch = false;
-            return next(new ErrorResponse('author does not exist', 404));
+            return res.status(404).send({
+                success: false,
+                msg: "Author Doesn't Exist"
+            });
         } else {
             isMatch = await bcrypt.compare(password, author.password);
-            console.log(isMatch);
         }
 
         if (!isMatch) {
-            return next(new ErrorResponse('Invalid credentials', 401));
+            return res.status(400).send({
+                success: false,
+                msg: "Invalid Credentials"
+            });
         } else {
             token = createToken(author._id);
             session.setItem("jwt", token);
-            return res.status(200).send({
-                success: true,
-                msg: "Logged In Successfully",
-                token
-            });
+            console.log(`${email} logged in`)
+            return res.json({ token: token })
         }
     } catch (err) {
-        return next(new ErrorResponse(err.name, err.code));
+        console.log(err)
+        return res.status(500).send({
+            success: false,
+            msg: "Internal Server Error"
+        });;
     }
 }
 
@@ -90,13 +108,17 @@ const logout = async (req, res, next) => {
                 msg: "Logged Out Successfully",
             });
         } else {
-            return res.status(404).send({
+            return res.status(400).send({
                 success: false,
                 msg: "Not Logged In"
             });
         }
     } catch (err) {
-        return next(new ErrorResponse(err.name, err.code));
+        console.log(err)
+        return res.status(500).send({
+            success: false,
+            msg: "Internal Server Error"
+        });
     }
 }
 
--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,1 +1,25 @@
-// isLoggedIn
+const jwt = require('jsonwebtoken');
+const Author = require('../models/authorModel');
+
+const checkLoggedIn = async (req, res, next) => {
+    const header = req.headers["authorization"];
+    const HL = header.split(' ');
+    const token = HL[1];
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            msg: "No token found. Please log in again.",
+        })
+    }
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        next()
+    } catch (err) {
+        res.status(401).json({
+            success: false,
+            msg: "Token is not valid. Please log in again.",
+        })
+    }
+}
+
+module.exports = { checkLoggedIn }
