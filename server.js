e8fce998805e6c2868ead9802c99ba88e19d3603
adityachandra1 <adityachandra9898@gmail.com>
2022-11-10 18:27:51 +0530

    base-server

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require('express')
-var app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.listen(5000, function () {
-    console.log("Started application on port % d", 5000);
+const { connectDB } = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+const TEST_PORT = 8080;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || TEST_PORT, async () => {
+    try {
+        console.log(`Listening at port: ${process.env.PORT || TEST_PORT}...`);
+        await connectDB();
+    } catch (err) {
+        console.log("Connection to Mongo unsuccessful...\n" + err);
+        process.exit();
+    }
 });
 
+app.get("/test", (req, res) => {
+    res.send("Hello World!");
+})
